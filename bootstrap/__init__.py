48b945b4daeac817039d86586f5f01a2da1d59d6
TomDonoghue <tdonoghue@ucsd.edu>
2020-07-23 22:39:13 -0700

    make pip installable

---
+++ bootstrap/__init__.py
@@ -0,0 +1,4 @@
+"""Bootstrap: basic bootstrapping for statistics."""
+
+from .version import __version__
+from .bootstrap import *
---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Bootstrap setup script."""
+
+import os
+from setuptools import setup, find_packages
+
+# Get the current version number from inside the module
+with open(os.path.join('bootstrap', 'version.py')) as version_file:
+    exec(version_file.read())
+
+# Load the required dependencies from the requirements file
+with open("requirements.txt") as requirements_file:
+    install_requires = requirements_file.read().splitlines()
+
+setup(
+    name = 'bootstrap',
+    version = __version__,
+    description = 'Basic bootstraps for statistics.',
+    long_description = 'Basic bootstraps for statistics.',
+    python_requires = '>=3.5',
+    author = 'Thomas Donoghue',
+    author_email = 'tdonoghue.research@gmail.com',
+    url = 'https://github.com/TomDonoghue/bootstraps',
+    packages = find_packages(),
+    license = 'MIT License',
+    install_requires = install_requires,
+    platforms = 'any',
+)
